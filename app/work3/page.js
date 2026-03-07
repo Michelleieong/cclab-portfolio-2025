cb0f608abb97b78b8bea01265f69c872c8400435
yangm <yangm122@newschool.edu>
2026-03-07 19:10:49 -0400

    删除废弃文件

--- app/work3/page.js
+++
@@ -1,9 +0,0 @@
-import SimpleBackPage from "../../components/SimpleBackPage";
-
-export const metadata = {
-  title: "Work 3 — woolyspace",
-};
-
-export default function WorkThreePage() {
-  return <SimpleBackPage title="Work 3" subtitle="Coming soon" />;
-}
